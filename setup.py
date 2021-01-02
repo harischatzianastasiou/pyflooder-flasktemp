5d8f223e787a131c354c8bfa522ac1d9a858ba64
tchatzian <75744098+tchatzian@users.noreply.github.com>
2021-01-02 22:17:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-        name='flask-temp',
+        name='flasktemp',
         version='1.0.0',
         description='contains some sample hello world code using Flask',
         author='tchatzian',
         author_email='harischatzi1988@gmail.com',
-        url='https://github.com/tchatzian/flask-temp',
-        packages=['flask-temp'],
+        url='https://github.com/tchatzian/flasktemp',
+        packages=['flasktemp'],
         include_package_data=True,
         zip_safe=False,
         install_requires=[
