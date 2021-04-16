7f50d8afb3121554c9cc63aa42ceee8aa714e9ef
tchatzian <75744098+tchatzian@users.noreply.github.com>
2021-04-16 19:36:03 +0000

    Update flasktemp.py

--- flasktemp/flasktemp.py
+++ flasktemp/flasktemp.py
@@ -13,17 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    
-    return """
-    <meta http-equiv="refresh" content={} /> 
-    This is a test <br><br>.""".format(args.refresh)
+    return "This is a test"
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("server", help="specify address and port")
-    parser.add_argument("refresh", help="specify refresh rate",
-                        type=int)
-    args = parser.parse_args()
-    app.config.update (
-        SERVER_NAME=args.server)
     app.run()
