ad33c28e68c00cf49fe40bfd45807a831b1f58d2
tchatzian <75744098+tchatzian@users.noreply.github.com>
2021-04-20 10:03:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
         install_requires=[
             'flask',
         ],
-        scripts=['flasktemp/flasktemp.py','flasktemp/mining.py']
+        scripts=['flasktemp/flasktemp.py']
 )
 
