92e7f2fdc1ea1822f08ef40beb760870eb0bec95
tchatzian <75744098+tchatzian@users.noreply.github.com>
2023-10-13 21:14:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from setuptools import setup
 setup(
         name='flasktemp',
         version='1.0.0',
-        description='contains some sample hello world code using Flask',
+        description='',
         author='tchatzian',
         author_email='harischatzi1988@gmail.com',
         url='https://github.com/tchatzian/flasktemp',
         packages=['flasktemp'],
         include_package_data=True,
         zip_safe=False,
-        install_requires=[
-            'flask',
-        ],
         scripts=['flasktemp/pyflooder.py']
 )
 
